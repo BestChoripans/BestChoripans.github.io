5ea80b5a9a6a63772b5c9a5ceb39cf38bc368967
BestChoripan <enderrfer@outlook.com>
2025-08-28 18:07:56 -0400

    chilean time and reduced script

--- script.js
+++ script.js
@@ -12,36 +12,19 @@ function clickSound() {
 
 setInterval(showTime, 1000);
 
-function showTime() { 
-	/* take the cl time and transform it to AM-PM format */
-	let time = new Date();
-	let hour = time.getHours();
-	let min = time.getMinutes();
-	let sec = time.getSeconds();
-	am_pm = " AM";
+function showTime() {
+    let time = new Date();
 
-	if (hour >= 12) {
-		if (hour > 12) hour -= 12;
-		am_pm = " PM";
-	} else if (hour == 0) {
-		hr = 12;
-		am_pm = " AM";
-	}
+    let options = {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,
+        timeZone: "America/Santiago"
+    };
 
-	hour =
-		hour < 10 ? "0" + hour : hour;
-		min = min < 10 ? "0" + min : min;
-		sec = sec < 10 ? "0" + sec : sec;
+    let currentTime = time.toLocaleString("en-US", options);
 
-	let currentTime =
-		hour +
-		":" +
-		min +
-		am_pm;
-
-		document.getElementById(
-			"clock"
-		).innerHTML = currentTime;
+    document.getElementById("clock").innerHTML = currentTime;
 }
 
 showTime();
